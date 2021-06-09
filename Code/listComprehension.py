e595327a5e342f85b2b3a5f3f03e97375bb54f17
Supreme-YS <dudtjr1225@gmail.com>
2021-06-09 18:30:58 +0900

    list comprehension & array

--- Code/listComprehension.py
+++ Code/listComprehension.py
@@ -42,3 +42,19 @@ for i in range(0, 4):
     for j in range(0, 3):
         Sum += list2[i][j]
 print("배열 내 값의 총 합은 : %d" % Sum)
+
+# List Comprehension
+# [수식 for 항목 in range() if 조건식]
+numList = [num for num in range(1, 6)]
+print(numList)
+numList = [num*num for num in range(1, 6)]
+print(numList)
+# 1부터 20까지 숫자 중 3의 배수 리스트
+numList = [num for num in range(1, 21) if num % 3 == 0]
+print(numList)
+
+# 2차원 리스트 컴프리헨션
+# [[수식 for 항목 in range()] for 항목 in range()]
+
+list2 = [[0 for _ in range(4)] for _ in range(3)]
+print(list2)
