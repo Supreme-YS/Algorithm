63c0f4d2ea4113d9d52dd096f8e79430f396dcf2
Supreme-YS <dudtjr1225@gmail.com>
2021-06-09 17:23:31 +0900

    update code

--- Code/functions.py
+++ Code/functions.py
@@ -50,7 +50,7 @@ func1() # 10 출력
 func2() # 10 출력
 ## 3행에서 global 예약어를 사용해서 a를 전역 변수로 지정했기 때문에 지역 변수 a는 존재하지 않는다. 따라서 모두 10 출력
 
-# Code02-04
+# Code02-04 ver1
 # 반환 값이 여러 개인 함수
 # 함수 선언 부분
 def multi(v1, v2):
@@ -70,4 +70,18 @@ if __name__ == "__main__":
     myList = multi(100, 200)
     Sum = myList[0]
     Diff = myList[1]
-    print("multi()에서 반환한 값 ==> %d, %d" % (Sum, Diff))
+    print("multi() ver1에서 반환한 값 ==> %d, %d" % (Sum, Diff))
+
+# Code02-04 ver2
+def multi(v1, v2):
+    res1 = v1 + v2
+    res2 = v1 - v2
+    return res1, res2 # 값을 두 개로 리턴한다면?
+
+# 전역 변수 선언 부분
+Sum, Diff = 0, 0
+
+# 메인 코드 부분 ( main 형태로 작성해 보았다. )
+if __name__ == "__main__":
+    Sum, Diff = multi(100, 200) # 두 개의 변수를 통해 값을 담으면 된다.
+    print("multi() ver2에서 반환한 값 ==> %d, %d" % (Sum, Diff))
