662f563bafb6aef7643e0d77083cafdc017b88c1
Supreme-YS <dudtjr1225@gmail.com>
2021-06-10 19:50:05 +0900

    linear list

--- Code/linearList3.py
+++ Code/linearList3.py
@@ -18,3 +18,23 @@ def insert_data(position, value):
 
 insert_data(5, "A")
 print(katok)
+
+# 데이터가 삭제되는 함수를 완성해보자
+katok = ["A", "B", "C", "D", "E"]
+
+def delete_data(position):
+    if position > len(katok) or position < 0:
+        print("범위를 벗어났습니다.")
+        return
+
+    katok[position] = None # 삭제하고자 하는 위치의 값 지우기
+    kLen = len(katok)
+
+    for i in range(position+1, kLen):
+        katok[i-1] = katok[i]
+        katok[i] = None
+
+    del(katok[kLen-1])
+
+delete_data(3)
+print(katok)
