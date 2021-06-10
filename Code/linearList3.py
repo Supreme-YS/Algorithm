fb8d0376459b52642f077f255370d9751d86dcb9
Supreme-YS <dudtjr1225@gmail.com>
2021-06-10 19:42:16 +0900

    linear list

--- Code/linearList3.py
+++ Code/linearList3.py
@@ -1,21 +1,20 @@
 # 데이터가 삽입되는 함수를 완성해보자
 katok = ["A", "B", "C", "D", "E"]
 
-def insert_data(position, friend):
+def insert_data(position, value):
 
-    if position < 0 or position > len(katok):
-        print("데이터를 삽입할 수 있는 범위를 벗어났습니다.")
+    if position > len(katok) or position < 0:
+        print("범위를 벗어났습니다.")
         return
-    katok.append(None) # 빈칸추가
-    kLen = len(katok)  # 배열의 현재 크기
+
+    katok.append(None)
+    kLen = len(katok)
 
     for i in range(kLen-1, position, -1):
         katok[i] = katok[i-1]
         katok[i-1] = None
 
-    katok[position] = friend # 지정한 위치에 친구 추가
+    katok[position] = value
 
-insert_data(2, "ACE")
-print(katok)
-insert_data(3, "YS")
+insert_data(5, "A")
 print(katok)
