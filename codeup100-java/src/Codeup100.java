4f4c09b73b2df58a54b20bd7119891e526c15e0d
Supreme-YS <dudtjr1225@gmail.com>
2021-09-10 18:39:21 +0900

    codeup basic 100

---
+++ codeup100-java/src/Codeup100.java
@@ -0,0 +1,20 @@
+
+public class Codeup100 {
+    public static void main(String[] args){
+        /* 1001 */
+        System.out.println("Hello");
+        /* 1002 */
+        System.out.println("Hello Java");
+        /* 1003 */
+        System.out.println("Hello");
+        System.out.println("Java");
+        /* 1004 */
+        System.out.println("'Hello, Java'");
+        /* 1005 */
+        System.out.println("\"Hello, Java\""); // escape 문자
+        /* 1006 */
+        System.out.println("\"!@#$%^&*()\"");
+        /* 1007 */
+
+    }
+}
