242899866fe28d0696c97f0436528613db9452c5
Supreme-YS <dudtjr1225@gmail.com>
2021-09-27 20:36:31 +0900

    review code :O

--- python-practice/linearList2.py
+++ python-practice/linearList2.py
@@ -1,21 +1,25 @@
 # 선형 리스트의 일반 구현
 katok = []
+print("빈 배열 생성 : ", katok)
+
 katok.append(None)
+print("빈 배열에 None 추가 : ", katok)
 
 배열길이 = len(katok)
+print("배열의 길이 : ", 배열길이)
+
 katok[배열길이-1] = '다현'
-print(katok)
+print("리스트 첫번째 자리에 데이터 삽입 : ", katok)
 
 # 새로운 데이터 삽입
 katok.append(None)
-print(katok)
+print("새로운 배열 칸 추가 : ", katok)
 
 배열길이 = len(katok)
 katok[배열길이-1] = '정연'
-print(katok)
+print("추가된 칸에 데이터 삽입 : ", katok)
 
 # 위 과정을 함수로 만들어 보자
-
 katok = []
 
 def add_data(friend):
@@ -29,4 +33,4 @@ add_data('쯔위')
 add_data('사나')
 add_data('지효')
 
-print(katok)
+print("함수를 이용한 배열 생성 : ", katok)
