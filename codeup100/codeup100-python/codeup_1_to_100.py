6a914dd754df3f49e926ea8806d6fc156f296733
Supreme-YS <dudtjr1225@gmail.com>
2021-09-28 21:07:46 +0900

    Update repository

---
+++ codeup100/codeup100-python/codeup_1_to_100.py
@@ -0,0 +1,30 @@
+#1
+print("hello")
+
+#2
+print("Hello World")
+
+#3
+print("Hello")
+print("World")
+
+#4
+print("'Hello'")
+
+#5
+print('"Hello World"')
+
+#6
+print("\"!@#$%^&*()\"")
+
+#7
+print("\"C:\Download\hello.cpp\"")
+
+#8
+print('\u250C\u252C\u2510')
+print('\u251C\u253C\u2524')
+print('\u2514\u2534\u2518')
+
+#10
+x = str(input())
+print(x)
