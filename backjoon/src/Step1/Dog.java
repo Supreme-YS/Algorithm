3293699da1e4e8b3ece965d4a6f914cf8733c0a3
Supreme-YS <dudtjr1225@gmail.com>
2021-10-18 18:33:02 +0900

    upload

---
+++ backjoon/src/Step1/Dog.java
@@ -0,0 +1,14 @@
+package Step1;
+
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+
+
