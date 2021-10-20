9b02b2d809540beb5b3a9c10d239cdc8acdd4ddf
Supreme-YS <dudtjr1225@gmail.com>
2021-10-20 12:59:29 +0900

    Use BufferedReader and BufferedWriter

---
+++ backjoon/src/Step3/UntilHap.java
@@ -0,0 +1,17 @@
+package Step3;
+
+import java.util.Scanner;
+
+public class UntilHap {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int hap = 0;
+        int cnt = input.nextInt();
+
+        for (int i=1; i <= cnt; i++){
+            hap = hap + i;
+        }
+        System.out.println(hap);
+    }
+}
