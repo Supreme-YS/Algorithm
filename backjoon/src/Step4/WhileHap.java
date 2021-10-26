92a8f63219303e5a62b06a28ae756940d7adbbb1
Supreme-YS <dudtjr1225@gmail.com>
2021-10-26 20:01:20 +0900

    Update

---
+++ backjoon/src/Step4/WhileHap.java
@@ -0,0 +1,16 @@
+package Step4;
+
+import java.util.Scanner;
+
+public class WhileHap {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        while(input.hasNextInt()) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
