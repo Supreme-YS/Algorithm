36a171f6a5a866c3111c4e583e21dea15b619957
Supreme-YS <dudtjr1225@gmail.com>
2021-10-27 18:15:23 +0900

    Update Code

--- backjoon/src/Step3/Hap2.java
+++ backjoon/src/Step3/Hap2.java
@@ -16,3 +16,21 @@ public class Hap2 {
         input.close();
     }
 }
+
+/*
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+
+    for(int i=1; i<=t; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int sum = a+b;
+      System.out.println("Case #"+i+": "+sum);
+    }
+  }
+}
+*/
