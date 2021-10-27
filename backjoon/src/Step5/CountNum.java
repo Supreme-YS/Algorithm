3eb2e542d0aa88cd5eea4e027a3745746a34f770
Supreme-YS <dudtjr1225@gmail.com>
2021-10-27 12:07:32 +0900

    Update

---
+++ backjoon/src/Step5/CountNum.java
@@ -0,0 +1,23 @@
+package Step5;
+
+import java.util.Scanner;
+
+public class CountNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int value = (input.nextInt() * input.nextInt() * input.nextInt());
+        String str = Integer.toString(value);
+        input.close();
+
+        for (int i = 0; i < 10; i++) {
+            int cnt = 0;
+            for (int j = 0; j < str.length(); j++){
+                if((str.charAt(j)-'0') == i){
+                    cnt++;
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+}
