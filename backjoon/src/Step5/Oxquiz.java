b3f4f147567584b241df439ef5227510d22e631b
Supreme-YS <dudtjr1225@gmail.com>
2021-11-08 15:26:26 +0900

    Update code

--- backjoon/src/Step5/Oxquiz.java
+++ backjoon/src/Step5/Oxquiz.java
@@ -5,15 +5,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+
 public class Oxquiz {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        // 테스트 케이스 수 입력 받기
         int test_case = Integer.parseInt(br.readLine());
 
         String[] arr = new String[test_case];
 
+        // 테스트 케이스 숫자만큼의 스트링 배열 생성
         for (int i = 0; i < test_case; i++){
             arr[i] = br.readLine();
         }
@@ -21,7 +24,7 @@ public class Oxquiz {
         for (int i = 0; i < test_case; i++){
 
             int cnt = 0;
-            int sum = 0;
+            int sum = 0; // 누적합
 
             for (int j = 0; j < arr[i].length(); j++) {
 
@@ -32,7 +35,6 @@ public class Oxquiz {
                 }
                 sum += cnt;
             }
-
             sb.append(sum).append('\n');
         }
         System.out.print(sb);
