760188c96a56af3d1d3e2cedf32df79e8d8b1f7b
Supreme-YS <dudtjr1225@gmail.com>
2021-11-15 19:21:28 +0900

    update

--- backjoon/src/Step11/ChessBoard.java
+++ backjoon/src/Step11/ChessBoard.java
@@ -3,6 +3,7 @@ package Step11;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ChessBoard {
@@ -20,7 +21,6 @@ public class ChessBoard {
                 sp = br.readLine().split("");
                 arr[i][j] = sp[j];
             }
-
         }
 
 
