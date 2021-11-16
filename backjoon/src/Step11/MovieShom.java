675264cab0a927f8043e793290a004aad3fbb2b7
Supreme-YS <dudtjr1225@gmail.com>
2021-11-16 15:39:51 +0900

    Update

---
+++ backjoon/src/Step11/MovieShom.java
@@ -0,0 +1,23 @@
+/* 1436번 : 영화감독 숌 */
+package Step11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MovieShom {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
--- backjoon/src/Step6/Hansu.java
+++ backjoon/src/Step6/Hansu.java
@@ -9,20 +9,17 @@ import java.io.InputStreamReader;
 public class Hansu {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.print(arithmetic_sequence(Integer.parseInt(br.readLine())));
-
+        System.out.print(hansu(Integer.parseInt(br.readLine())));
     }
 
-    public static int arithmetic_sequence(int num) {
-        int cnt = 0; // 한수 카운팅
-
+    public static int hansu(int num) {
+        int cnt = 0; // 한수 갯수
         if (num < 100) {
-            return num;
+            return num; // 100보다 작으면 그대로 리턴
         }
 
-        else {
-            cnt = 99;
+        else { // 100보다 크면
+            cnt = 99; // 일단 99개 깔고 시작
             if (num == 1000) { // 예외처리 필수
                 num = 999;
             }
@@ -30,14 +27,14 @@ public class Hansu {
             for (int i = 100; i <= num; i++) {
                 int hun = i / 100; // 백의 자릿수
                 int ten = (i / 10) % 10; // 십의 자릿수
-                int one = i % 10;
+                int one = i % 10; // 일의 자릿수
 
-                if ((hun - ten) == (ten - one)) { // 각 자릿수가 수열을 이루면
-                    cnt++;
+                if ((hun - ten) == (ten - one)) { // 각 자릿수의 차가 수열을 이루면
+                    cnt++; // 한수 카운트 증가 후
                 }
             }
         }
-        return cnt;
+        return cnt; // 카운트 값 리턴
     }
 }
 
