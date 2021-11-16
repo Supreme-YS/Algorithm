d9340b4514f4ac1c4e9f0f38fb11db625fdace83
Supreme-YS <dudtjr1225@gmail.com>
2021-11-16 17:25:14 +0900

    update

--- backjoon/src/Weekly/AplusB7.java
+++ backjoon/src/Weekly/AplusB7.java
@@ -1,4 +1,23 @@
 package Weekly;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class AplusB7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i=1; i <= testCase; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println("Case #"+i+":"+" "+(a+b));
+        }
+    }
 }
