f037db075e4c644c5c3d36e471e75ac0027193d2
Supreme-YS <dudtjr1225@gmail.com>
2021-11-18 20:26:34 +0900

    Solve 2441

---
+++ backjoon/src/Daily/P2439.java
@@ -0,0 +1,26 @@
+package Daily;
+
+import java.io.*;
+
+public class P2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=N-i; j++) {
+                bw.write(" ");
+            }
+
+            for (int k=0; k<i; k++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+
+    }
+}
