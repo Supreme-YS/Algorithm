6ee2ab92dccfabe2961586aeb822ed8ec912283b
Supreme-YS <dudtjr1225@gmail.com>
2021-11-21 18:32:28 +0900

    Counting Sort

--- backjoon/src/Step12/P10989.java
+++ backjoon/src/Step12/P10989.java
@@ -2,30 +2,28 @@ package Step12;
 
 import java.io.*;
 
+/* 중복되는 숫자가 있다 */
+/* 중복되는 숫자가 없을 땐 boolean형 배열을 사용하면 되지만
+중복되는 숫자는 빈도수를 기반함
+ */
 public class P10989 {
     public static void main(String[] args) throws IOException {
-        // 수의 범위 (0 ~ 10000) 사실상 0은 제외
-        int[] cnt = new int[10001];
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            // 해당 인덱스의 값을 1 증가
-            cnt[Integer.parseInt(br.readLine())] ++;
-        }
-
-        br.close();
-
         StringBuilder sb = new StringBuilder();
 
-        // 0은 입력범위에서 없으므로 1부터 시작
-        for(int i = 1; i < 10001; i++){
-            // i 값이 개수가 0 이 될 때 까지 출력 (빈도수를 의미)
-            while(cnt[i] > 0){
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[10001]; // 수의 범위 배열
+
+        // 숫자의 빈도 인덱스를 증가시킨다.
+        for(int i=0; i<N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        } // 예. 숫자 1이면 arr[1]이 0에서 1로 증가, 다음 숫자도 1이면 1에서 2로 됨
+
+        // 배열을 돌자!
+        for(int i=1; i < 10001; i++ ){ // 배열은 전체를 다 돌면서 탐색
+            while (arr[i] > 0){ // 배열 인덱스의 증가 됐던 빈도수를 하나씩 깎으면서 0이 될때까지 반복
                 sb.append(i).append('\n');
-                cnt[i]--;
+                arr[i]--;
             }
         }
         System.out.println(sb);
