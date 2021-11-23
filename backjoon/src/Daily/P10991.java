6de3b4c795d7ab17f69930f0a5fb7b19340eedbf
Supreme-YS <dudtjr1225@gmail.com>
2021-11-23 20:01:08 +0900

    Solving...

--- backjoon/src/Daily/P10991.java
+++ backjoon/src/Daily/P10991.java
@@ -9,10 +9,19 @@ public class P10991 {
 
         int N = Integer.parseInt(br.readLine());
 
-        for (int i=0; i<N; i++) {
+        for (int i = 1; i <= N; i++) {
 
-            for (int j=0; j<i+1; j++) {
-                bw.write("*");
+            for (int j = 1; j <= N - i; j++) {
+                bw.write(" ");
+            }
+
+            for (int k = 1; k <= 2*i-1; k++) {
+
+                if (i % 2 == 1) {
+                    bw.write("*");
+                } else {
+                    bw.write("-");
+                }
             }
             bw.newLine();
         }
--- backjoon/src/Step12/P2108.java
+++ backjoon/src/Step12/P2108.java
@@ -18,13 +18,14 @@ public class P2108 {
         int[] arr = new int[N];
 
         /* 최빈값을 위한 카운트 배열 */
-        int[] count = new int[8001];
+//        int[] count = new int[8001]; // 음수처리 문제가 발생
 
         for (int i=0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            count[arr[i]]++; // 입력과 동시에 카운트 배열 증가
+//            count[arr[i]]++; // 입력과 동시에 카운트 배열 증가
              }
         /* ----------------입력---------------- */
+
         /* 평균 구현 */
         int sum = 0;
         for (int value : arr) {
@@ -37,25 +38,28 @@ public class P2108 {
         Arrays.sort(arr);
         System.out.println(arr[(N/2)]);
 
-        /* 최빈값 */
-        ArrayList<Integer> mode = new ArrayList<>();
+        /* 최빈값 구현 */
 
-        for (int i=1; i < 8001; i++) {
-            if (count[i] >= 2) {
-                mode.add(count[i]);
-            }
-        }
-        System.out.println(mode);
+
+        /* 범위 구현 */
+        int range = arr[arr.length-1] - arr[0];
+        System.out.println(range);
+
+
+//        ArrayList<Integer> mode = new ArrayList<>();
+//
+//        for (int i=1; i < 8001; i++) {
+//            if (count[i] >= 2) {
+//                mode.add(count[i]);
+//            }
+//        }
+//        System.out.println(mode);
 //        Collections.sort(mode);
 //        if (mode.size() >= 2) {
 //            System.out.println(mode.get(1));
 //        } else {
 //            System.out.println(mode.get(0));
 //        }
-
-        /* 범위 */
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
 
 
 
