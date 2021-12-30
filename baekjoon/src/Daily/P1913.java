a0e3d7bed497d807a1a97e6123af0b180f41587b
Supreme-YS <dudtjr1225@gmail.com>
2021-12-30 18:07:55 +0900

    Update

---
+++ baekjoon/src/Daily/P1913.java
@@ -0,0 +1,16 @@
+package Daily;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1913 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int target = Integer.parseInt(br.readLine());
+
+        
+    }
+}
