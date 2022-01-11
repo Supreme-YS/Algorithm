c37a62d98c0f26e4b7128f63d1c02cb4af82cf01
Supreme-YS <dudtjr1225@gmail.com>
2022-01-11 16:57:33 +0900

    Greedy Example

---
+++ baekjoon/src/greedy/UntilOne.java
@@ -0,0 +1,26 @@
+package greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class UntilOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while (N > 1) {
+            if (N % K == 0) {
+                N = N / K;
+                cnt++;
+            } else {
+                N = N - 1;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
