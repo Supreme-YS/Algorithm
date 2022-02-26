58306f0742f942b1b7b19ae749135c517e64a95a
Supreme-YS <dudtjr1225@gmail.com>
2022-02-26 16:59:18 +0900

    Update Three Operation

--- baekjoon/src/java100/ThreeOperation.java
+++ baekjoon/src/java100/ThreeOperation.java
@@ -10,5 +10,13 @@ public class ThreeOperation {
         int result = age <= 20 ? age * 100 : age;
 
         System.out.println("result = " + result);
+
+        // 12시 보다 작으면 오전, 12시보다 크면 오후를 삼항연산자를 사용해서 만들어라
+
+        int time = 11;
+
+        String value = time < 12 ? value = "오전" : "오후";
+
+        System.out.println("value = " + value);
     }
 }
